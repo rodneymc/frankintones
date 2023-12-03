d5b138fd8b27ff3ab8ee30a08a138f25fb32a600
rodney <rodney@daftdroid.com>
2023-12-03 10:08:58 +0000

    annote note.py with comments, use MIDI numbering
    MIDI numbering starts at note C and octave 1. As we were previously
    numbering the octaves from zero, we had to add 12 to the value. Now
    MIDI number and our numbering align.

--- note.py
+++ note.py
@@ -19,15 +19,28 @@ class Note:
         self.freq = tuning.reffreq * tuning.refinterval ** (number / tuning.refdivisor)
         self.name = name
         self.harmonics = tuning.harmonics
-        self.phases = tuning.phases
-            
+        self.phases = tuning.phases #List of phase angles in radians
+        self.wavetable = None    #Pre-prepared long-duration of tone
+        self.rt_wavetable = None #Pre-prepared wavetable list per phase for realtime
+
+    # Get the integer absolute note number. This is now synchronized to use the same
+    # scheme as MIDI numbering, which starts at C1. C is the first note in the octave
+    # the first octave is 1.
+        
     def name_to_number_abs(name):
         namesplit = re.findall("[A-G]#*|[0-9]", name)
-        return int(namesplit[1]) * 12 + Note.NOTES.index(namesplit[0])
-    
-    def name_to_number_midi(name):
-        return Note.name_to_number_abs(name) + 12
+        return (int(namesplit[1]) + 1) * 12 + Note.NOTES.index(namesplit[0])
 
+    #See above    
+    def name_to_number_midi(name):
+        return Note.name_to_number_abs(name)
+
+    # Generates wavetable data for a given duration
+    # Inputs:
+    #  duration - Length of wavetable in seconds
+    #  trim - whether to adjust the length to an exact number of samples
+    #  phase - phase offset in radians. If None, then all the phases
+    #          listed in self.phases will be superimposed.
     def get_data(self, duration, trim=True, phase=None):
 
         # Interprete phase==None as all the phases
@@ -69,22 +82,29 @@ class Note:
                 h_count += 1
         return result
 
+    # Iterate over the phase list yeilding a wavetable for each one.
+    # This uses get_data which generates data each time.
+    # duration - seconds
+    # trim - see get_data()
     def get_phases(self, duration, trim):
         for phase in self.phases:
             yield self.get_data(duration, trim, phase)
 
+    # Prepare a wavetable of a specific duration with all the phases.
+    # This is used typically for a long duration wavetable and may become
+    # redundant if sufficient real-time performance can be acheived.
     def pre_prepare(self, duration, trim=True):
         self.wavetable = self.get_data(duration, trim)
 
+    # Get the pre-prepared wavetable - pre_prepared must have previously been called.
     def get_prepared_data(self):
+        if self.wavetable == None:
+            raise UserWarning("Attempting to use wavetable uninitialized")
         return self.wavetable
 
+    # Blocking function which simply generates a wavetable for a given duration
+    # and plays it. Returns when the note has finished playing.
     def play(self, duration):
         data = self.get_data(duration)
         sd.play(data, self.config.sample_rate)
-        sd.wait()
-
-    def play_buffer(buffer, config):
-        data = np.array(buffer)
-        sd.play(data, config.sample_rate)
         sd.wait()
