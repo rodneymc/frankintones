b62f8f49ef583af9e7074b9c7af4db20f692f8a2
rodney <rodney@daftdroid.com>
2023-12-04 03:06:13 +0000

    add framework for individually timing each sinewave
    Now instead of a list of active notes we keep a list of
    active sinewaves (with which notes they are associated)

--- notebank.py
+++ notebank.py
@@ -4,15 +4,18 @@ import numpy as np
 from note import Note
 from time import time, sleep
 import sounddevice as sd
+import threading
 
 
 class Notebank:
     def __init__(self, config, tuning):
         self.config = config
-        self.current_note_mask = 0
-        self.current_chord = None
         self.out_stream = None
         self.start_idx = 0
+        self.active_sinewaves = {}
+        self.sinewaves_pending_start = {}
+        self.sinewaves_pending_stop = {}
+        self.sine_lists_mutex = threading.Lock()
 
         # Start from Midi note number zero so we can just
         # have a simple array.
@@ -36,23 +39,64 @@ class Notebank:
         t = (self.start_idx + np.arange(frames)) / self.config.sample_rate
         #print (t)
         t = t.reshape(-1, 1)
-        done_first = False
 
-        for note_num in range(len(self.notelist)):
-            if (1 << note_num) & self.current_note_mask != 0: 
-                for sinewave in self.notelist[note_num].get_sinewave_attributes():
-                    if not done_first:
-                        outdata[:] = sinewave.amp * np.sin(t*sinewave.freq + sinewave.phase)
-                        done_first = True
-                    else:
-                        outdata[:] += sinewave.amp * np.sin(t*sinewave.freq + sinewave.phase)
+        outdata[:] = np.zeros((frames, 1))
+
+        # TODO this is an outline framework for allowing the sinewaves to
+        # be switched on and off separately. At present it actuall just does
+        # them all at the same time.
+        with self.sine_lists_mutex:
+            for note_num, sw_list in self.sinewaves_pending_start.items():
+                # Transfer the sinewaves that can be started this frame sequence
+                # to the active list.
+                # Start by seeing if there is already a lookup for this note in 
+                # the active list.
+                active_sw_list_for_note = self.active_sinewaves.get(note_num)
+                if active_sw_list_for_note == None:
+                    self.active_sinewaves[note_num] = active_sw_list_for_note = []
+                
+                sw_moved_to_active_list = []
+                for sw in sw_list[::-1]: # iterate backwards for efficient removal
+                    if sw not in active_sw_list_for_note:
+                        active_sw_list_for_note.append(sw)
+                        sw_moved_to_active_list.append(sw)
+                for sw in sw_moved_to_active_list:
+                    sw_list.remove(sw)
+
+                for note_num, sw_list in self.sinewaves_pending_stop.items():
+                    ## TODO timing
+                    if self.active_sinewaves.get(note_num):
+                        self.active_sinewaves.pop(note_num)
+
+
+        for sinewave_list in self.active_sinewaves.values():
+            for sinewave in sinewave_list:
+                outdata[:] += sinewave.amp * np.sin(t*sinewave.freq + sinewave.phase)
         self.start_idx += frames
 
     def note_on(self, midi_key):
         note = self.notelist[midi_key]
         eprint("Note on %d %s %dHz" %(midi_key, note.name, note.freq))
         
-        self.current_note_mask |= (1 << midi_key)
+        with self.sine_lists_mutex:
+            # Remove this note from the stop queue.
+            if self.sinewaves_pending_stop.get(midi_key):
+                self.sinewaves_pending_stop.pop(midi_key)
+
+            # Check for any sinewaves for this note that are still playing,
+            # we won't be adding those.
+
+            already_playing = self.active_sinewaves.get(midi_key)
+
+            # Add all the sinewaves for that note to the start queue, unless
+            # they happen to be playing already.
+            new_sw_list = []
+            for sw in self.notelist[midi_key].get_sinewave_attributes():
+                if already_playing and sw in already_playing:
+                    continue
+                new_sw_list.append(sw)
+            self.sinewaves_pending_start[midi_key] = new_sw_list
+
         if self.out_stream == None:
             self.out_stream = sd.OutputStream(blocksize=int(self.config.sample_rate/100), 
                         device = 5,
@@ -63,13 +107,11 @@ class Notebank:
 
     def note_off(self, midi_key):
         eprint("Note off %d" %midi_key)
-        self.current_note_mask &= ~ (1 << midi_key)
-        if self.current_note_mask == 0 and self.out_stream != None:
-            ## TODO don't stop abruptly
-            self.out_stream.stop()
-            self.out_stream.close()
-            self.out_stream = None
-        
+        with self.sine_lists_mutex:
+            self.sinewaves_pending_stop[midi_key] = self.active_sinewaves.get(midi_key)
+            # Cancel any sinewaves from this note that have been queued to start already
+            if self.sinewaves_pending_start.get(midi_key):
+                self.sinewaves_pending_start.pop(midi_key)
 
     def stop(self):
         if self.out_stream:
